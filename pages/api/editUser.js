94a749d56ae59f657a2bd1293b6684410777e9d6
Tri <trimpham98@gmail.com>
2024-08-25 23:21:29 -0700

    Initial commit

---
+++ pages/api/editUser.js
@@ -0,0 +1,19 @@
+import { openDb } from "../../lib/db";
+
+export default async function handler(req, res) {
+  if (req.method === "PUT") {
+    const db = await openDb();
+    const { id, firstName, lastName, grade, mainInstrument } = req.body;
+    try {
+      await db.run(
+        "UPDATE users SET first_name = ?, last_name = ?, grade = ?, main_instrument = ? WHERE id = ?",
+        [firstName, lastName, grade, mainInstrument, id]
+      );
+      res.status(200).json({ message: "Student updated successfully" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
