ab85f1c1c11cb13bd1d6eed0bb9b1b2bfc32d2bf
Tri <trimpham98@gmail.com>
2024-08-27 08:12:32 -0700

    Rename header to SJZ Progressions

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-gray-800 text-white p-4">
-      <h1 className="text-2xl font-bold text-blue-400">GrooveGamer</h1>
+      <h1 className="text-2xl font-bold text-blue-400">San Jose Jazz Progressions</h1>
     </header>
   );
 }
