eda0cf89115414ea1b1164900628e7275670fb2a
Tri <trimpham98@gmail.com>
2024-08-30 21:25:43 -0700

    Add function that directs to Dashboard tab

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <header className="bg-gray-900 text-gray-300 p-4">
-      <h1 className="text-2xl font-bold text-gray-100">San Jose Jazz Progressions</h1>
+      <h1 className="text-2xl font-bold text-gray-100">
+        San Jose Jazz Progressions
+      </h1>
     </header>
   );
 }
