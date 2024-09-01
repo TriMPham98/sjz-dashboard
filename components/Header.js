099d63febfcd665b0adccf46788a3ed70c68b7a0
Tri <trimpham98@gmail.com>
2024-09-01 09:04:45 -0700

    Enlarge text to 5xl

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,7 @@ export default function Header({ onOpenDashboard }) {
     <header className="bg-gray-900 text-gray-300 p-4 flex justify-between items-center">
       <button
         onClick={onOpenDashboard}
-        className="text-2xl font-bold text-gray-100 hover:text-gray-300 transition-colors"
-      >
+        className="text-5xl font-bold text-gray-100 hover:text-gray-300 transition-colors">
         San Jose Jazz Progressions
       </button>
     </header>
