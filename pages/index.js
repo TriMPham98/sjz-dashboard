77153f32ed126368e6057c0da48fde803e5da9bd
Tri <trimpham98@gmail.com>
2024-08-31 22:06:39 -0700

    Refactor tab title

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-black text-gray-300">
       <Head>
-        <title>SJZ - Groove Gamer</title>
+        <title>San Jose Jazz Progressions</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
