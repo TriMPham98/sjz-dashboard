ae583e465eaf7f1696558a141670da97ceb4f416
Tri <trimpham98@gmail.com>
2024-09-01 08:53:52 -0700

    Move Download button next to subheader

--- components/Resources.js
+++ components/Resources.js
@@ -11,9 +11,17 @@ const Resources = () => {
     <div className="container mx-auto px-4">
       <h1 className="text-4xl font-bold mb-8 text-gray-100">Resources</h1>
       <div className="bg-gray-800 p-6 rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold mb-4 text-gray-100">
-          Bridges Rock Band Enrollment Form
-        </h2>
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-2xl font-bold text-gray-100">
+            Bridges Rock Band Enrollment Form
+          </h2>
+          <a
+            href="/BridgesRockBandEnrollment.pdf"
+            download
+            className="bg-gray-700 hover:bg-gray-600 text-gray-100 font-bold py-2 px-4 rounded">
+            Download
+          </a>
+        </div>
         <div className="relative" style={{ paddingTop: "141.42%" }}>
           {isLoading && (
             <div className="absolute inset-0 flex items-center justify-center">
@@ -26,14 +34,6 @@ const Resources = () => {
             style={{ border: "none" }}
             onLoad={handleIframeLoad}
             title="Bridges Rock Band Enrollment"></iframe>
-        </div>
-        <div className="mt-4">
-          <a
-            href="/BridgesRockBandEnrollment.pdf"
-            download
-            className="bg-gray-700 hover:bg-gray-600 text-gray-100 font-bold py-2 px-4 rounded">
-            Download
-          </a>
         </div>
       </div>
     </div>
