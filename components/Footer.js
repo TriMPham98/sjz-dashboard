fe8a0b4e2d1695dd9ec2ae344b4fb70a8e62b630
Tri <trimpham98@gmail.com>
2024-09-01 23:05:30 -0700

    Space evenly for footer items

--- components/Footer.js
+++ components/Footer.js
@@ -4,21 +4,34 @@ const Footer = () => {
   return (
     <footer className="bg-black text-white py-6 border-t border-gray-800">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-around items-start">
+        <div className="flex flex-col md:flex-row justify-evenly items-start">
           <div className="mb-4 md:mb-0">
-            <h3 className="text-xl font-bold mb-2">Contact Us</h3>
+            <h3 className="text-xl font-bold mb-2">Instructor Contacts</h3>
             <p>Tri Pham - trip@sanjosejazz.com</p>
             <p>Rey Rafanan - reyr@sanjosejazz.org</p>
           </div>
           <div>
             <h3 className="text-xl font-bold mb-2">Links</h3>
-            <a
-              href="https://sanjosejazz.org/educations/progressions/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-400 hover:text-white transition-colors duration-300">
-              Visit Our Website
-            </a>
+            <ul className="space-y-2">
+              <li>
+                <a
+                  href="https://sanjosejazz.org/educations/progressions/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-gray-400 hover:text-white transition-colors duration-300">
+                  San Jose Jazz - Progressions
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://bridges.fmsd.org/academics/out-of-school-time-programs-athletics/san-jose-jazz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-gray-400 hover:text-white transition-colors duration-300">
+                  Bridges Afterschool Programs
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
