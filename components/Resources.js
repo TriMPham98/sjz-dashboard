7dd15bc39bfa99df613714a0787e3e01d8de15c3
Tri <trimpham98@gmail.com>
2024-10-11 12:01:46 -0700

    Resolve apostrophe linting error

--- components/Resources.js
+++ components/Resources.js
@@ -15,7 +15,7 @@ const Resources = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4 border border-gray-800 flex flex-col">
           <h2 className="text-2xl font-bold mb-4 text-white">
-            Songs We're Learning
+            Songs We&apos;re Learning
           </h2>
           <div className="flex-grow">
             <iframe
